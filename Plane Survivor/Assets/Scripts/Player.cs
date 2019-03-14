3536b37411a4230ee900465a60e299a78a84cd3f
CristianAlarcon <calarconsan@uoc.edu>
2019-03-15 00:32:48 +0100

    canvis 14 març
    Afegides més animacions, afegit control esquerra i dreta, ara ja hi ha
    transició entre l'animació de moviment i descans, a més segons on giris
    el personatge apunta a esquerra o dreta, però al principi hi ha un salt
    que encara no sé ben bé perquè passa...

---
+++ Plane Survivor/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D myRigidBody;
+    [SerializeField] float moveSpeed = 10f;
+    // Use this for initialization
+    void Start ()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetAxis("Horizontal")!= 0)
+        {
+            gameObject.GetComponent<Animator>().SetBool("Running", true);
+        }
+        else
+        {
+            gameObject.GetComponent<Animator>().SetBool("Running", false);
+        }
+        Run();
+        FlipSprite();
+	}
+
+    private void Run()
+    {
+        var controlHoritzontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+        Vector2 playerVelocity = new Vector2(controlHoritzontal * moveSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        bool hihaMovimentHoritzontal = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (hihaMovimentHoritzontal)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x) * 1.25f, 1.25f);
+        }
+    }
+}
