26ac653629a9a375faf22d974ea8fcee1315ce57
CristianAlarcon <calarconsan@uoc.edu>
2019-03-28 16:25:26 +0100

    Personatge nou, fons amb relleu, càmera millorada
    He canviat al personatge per una noia que he trobat que està millor
    animada i millora l'aspecte del joc, els fons tot i no ser gran cosa
    encara almenys ara tenen relleu, la lava ja puja, hi ha animacions per
    al salt i la mort.
    
    A més hi ha tres càmeres, una propera quan estàs un parell de segons
    quiet i s'apropa i dues càmeres segons la direcció cap on t'estiguis
    movent.

--- Plane Survivor/Assets/Scripts/Player.cs
+++ Plane Survivor/Assets/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour {
     Animator myAnimator;
     bool CanJump = false;
     bool isAlive = true;
+    bool facingRight = true;
     CapsuleCollider2D myBodyCollider;
     BoxCollider2D myFeet;
     [SerializeField] float moveSpeed = 10f;
@@ -35,20 +36,58 @@ public class Player : MonoBehaviour {
 
     private void Run()
     {
+        /*if (Input.GetKey("left"))
+        {
+            var controlHoritzontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+            Vector2 playerVelocity = new Vector2(controlHoritzontal * moveSpeed, myRigidBody.velocity.y);
+            myRigidBody.velocity = playerVelocity;
+            facingRight = false;
+            myAnimator.SetBool("Running", true);
+        }
+        else if (Input.GetKey("right"))
+        {
+            myRigidBody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime, myRigidBody.velocity.y);
+            facingRight = true;
+            myAnimator.SetBool("Running", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Running", false);
+        }
+        */
         var controlHoritzontal = Input.GetAxis("Horizontal") * Time.deltaTime;
         Vector2 playerVelocity = new Vector2(controlHoritzontal * moveSpeed, myRigidBody.velocity.y);
         myRigidBody.velocity = playerVelocity;
         bool hihaMovimentHoritzontal = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
-        myAnimator.SetBool("Running", hihaMovimentHoritzontal);
+        if (hihaMovimentHoritzontal)
+        {
+            facingRight = Input.GetAxis("Horizontal") > Mathf.Epsilon;
+        }
+        myAnimator.SetBool("Facing Right", facingRight);
+        if (facingRight)
+        {
+            myAnimator.SetBool("Running Right", hihaMovimentHoritzontal);
+        }
+        else
+        {
+            myAnimator.SetBool("Running Left", hihaMovimentHoritzontal);
+        }
     }
 
     private void Jump()
     {
-        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
-        if (Input.GetButtonDown("Jump"))
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
-            Vector2 JumpVelocity = new Vector2(0f, JumpForce);
-            myRigidBody.velocity = JumpVelocity;
+            myAnimator.SetBool("Jump1", true);
+        }
+        else
+        {
+            myAnimator.SetBool("Jump1", false);
+            if (Input.GetButtonDown("Jump"))
+            {
+                Vector2 JumpVelocity = new Vector2(0f, JumpForce);
+                myRigidBody.velocity = JumpVelocity;
+            }
         }
     }
 
@@ -74,7 +113,7 @@ public class Player : MonoBehaviour {
         bool hihaMovimentHoritzontal = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon; //epsilon és el float més petit, perquè si comparem amb 0 pot donar error
         if (hihaMovimentHoritzontal)
         {
-            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x) * 1.25f, 1.25f); //1.25 perquè és l'escala del nostre personatge, el sign ens indica si esquerra o dreta
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x) * 1f, 1f); //1.25 perquè és l'escala del nostre personatge, el sign ens indica si esquerra o dreta
         }
     }
 
